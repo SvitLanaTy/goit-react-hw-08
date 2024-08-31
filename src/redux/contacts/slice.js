09f039caac49763f991deb175a1df472195c74c8
SvitLanaTy <stymchenko@gmail.com>
2024-08-31 18:42:00 +0200

    Update slice.js

--- src/redux/contacts/slice.js
+++ src/redux/contacts/slice.js
@@ -26,21 +26,18 @@ const contactsSlice = createSlice({
       .addCase(fetchContacts.pending, handlePending)
       .addCase(fetchContacts.fulfilled, (state, action) => {
         state.isLoading = false;
-        state.error = null;
         state.items = action.payload;
       })
       .addCase(fetchContacts.rejected, handleRejected)
       .addCase(addContact.pending, handlePending)
       .addCase(addContact.fulfilled, (state, action) => {
         state.isLoading = false;
-        // state.error = null;
         state.items.push(action.payload);
       })
       .addCase(addContact.rejected, handleRejected)
       .addCase(deleteContact.pending, handlePending)
       .addCase(deleteContact.fulfilled, (state, action) => {
         state.isLoading = false;
-        state.error = null;
         state.items = state.items.filter(
           (contact) => contact.id !== action.payload.id
         );
@@ -48,6 +45,5 @@ const contactsSlice = createSlice({
       .addCase(deleteContact.rejected, handleRejected);
   },
 });
-
 
 export const contactsReducer = contactsSlice.reducer;
